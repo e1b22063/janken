726c9951b7d8fd012dae69861d61009c8a970238
e1b22063 <e1b22063@oit.ac.jp>
2024-10-04 13:43:18 +0900

    対戦実装

--- src/main/java/oit/is/z2453/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2453/kaizi/janken/controller/JankenController.java
@@ -1,10 +1,9 @@
 package oit.is.z2453.kaizi.janken.controller;
 
-import org.attoparser.dom.Text;
+import oit.is.z2453.kaizi.janken.model.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,6 +17,16 @@ public class JankenController {
   @PostMapping("/janken")
   public String janken(@RequestParam String name, ModelMap model) {
     model.addAttribute("name", name);
+    return "janken.html";
+  }
+
+  @GetMapping("/jankengame")
+  public String jankengame(@RequestParam String hand, ModelMap model) {
+    Janken jn = new Janken();
+    jn.setHand(hand);
+    String result;
+    result = jn.judge(model);
+    model.addAttribute("result", result);
     return "janken.html";
   }
 }
