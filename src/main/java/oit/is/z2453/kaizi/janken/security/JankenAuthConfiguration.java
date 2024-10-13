65d25a948fd9d00388f04279764b2fbe0ace08e7
e1b22063 <e1b22063@oit.ac.jp>
2024-10-14 00:52:31 +0900

    認証処理の実装

---
+++ src/main/java/oit/is/z2453/kaizi/janken/security/JankenAuthConfiguration.java
@@ -0,0 +1,46 @@
+package oit.is.z2453.kaizi.janken.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class JankenAuthConfiguration {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.formLogin(login -> login
+        .permitAll())
+        .logout(logout -> logout
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/")) // ログアウト後に / にリダイレクト
+        .authorizeHttpRequests(authz -> authz
+            .requestMatchers(AntPathRequestMatcher.antMatcher("/janken/**"))
+            .authenticated() // /janken/以下は認証済みであること
+            .requestMatchers(AntPathRequestMatcher.antMatcher("/**"))
+            .permitAll())// 上記以外は全員アクセス可能
+        .csrf(csrf -> csrf
+            .ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/*")))// h2-console用にCSRF対策を無効化
+        .headers(headers -> headers
+            .frameOptions(frameOptions -> frameOptions
+                .sameOrigin()));
+    return http.build();
+  }
+
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+    UserDetails user1 = User.withUsername("user1")
+        .password("{bcrypt}$2y$05$lzJrrsLQR90YPpzh/5FaOuKVWDk8SCm3XTbugIzZMJsOP4lDYdJ/6").roles("USER").build();
+    UserDetails user2 = User.withUsername("user2")
+        .password("{bcrypt}$2y$05$O5hhLxpWvPkmigexZh8R6uFmrIK.YXmXtm42aAexuw7Enid5twB2i").roles("USER").build();
+    return new InMemoryUserDetailsManager(user1, user2);
+  }
+
+}
