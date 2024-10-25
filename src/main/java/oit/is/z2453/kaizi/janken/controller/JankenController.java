447c88e949e52c9fcfa393b4c37d7a8161f8dd55
e1b22063 <e1b22063@oit.ac.jp>
2024-10-25 14:17:17 +0900

    match

--- src/main/java/oit/is/z2453/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2453/kaizi/janken/controller/JankenController.java
@@ -1,46 +1,51 @@
 package oit.is.z2453.kaizi.janken.controller;
 
-import oit.is.z2453.kaizi.janken.model.Entry;
 import oit.is.z2453.kaizi.janken.model.Janken;
 
 import java.security.Principal;
-//import java.util.ArrayList;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import oit.is.z2453.kaizi.janken.model.User;
+import oit.is.z2453.kaizi.janken.model.Match;
 import oit.is.z2453.kaizi.janken.model.UserMapper;
+import oit.is.z2453.kaizi.janken.model.MatchMapper;
 
 @Controller
+@RequestMapping("/")
 public class JankenController {
 
   @Autowired
-  private Entry room;
   UserMapper userMapper;
 
-  @GetMapping("/janken")
-  @Transactional
+  @Autowired
+  MatchMapper matchMapper;
+
+  @GetMapping("janken")
   public String janken(Principal prin, ModelMap model) {
     String loginUser = prin.getName();
-    this.room.addUser(loginUser);
-    model.addAttribute("room", this.room);
+    ArrayList<User> allUsers = userMapper.selectAllUser();
+    ArrayList<Match> allMatches = matchMapper.selectAllMatches();
+    model.addAttribute("allUsers", allUsers);
+    model.addAttribute("allMatches", allMatches);
     model.addAttribute("name", loginUser);
     return "janken.html";
   }
 
-  @PostMapping("/janken")
+  @PostMapping("janken")
   public String janken(@RequestParam String name, ModelMap model) {
     model.addAttribute("name", name);
     return "janken.html";
   }
 
-  @GetMapping("/jankengame")
+  @GetMapping("jankengame")
   public String jankengame(@RequestParam String hand, ModelMap model) {
     Janken jn = new Janken();
     jn.setHand(hand);
@@ -48,5 +53,43 @@ public class JankenController {
     result = jn.judge(model);
     model.addAttribute("result", result);
     return "janken.html";
+  }
+
+  @GetMapping("match")
+  public String match(Principal prin, @RequestParam int id, ModelMap model) {
+    String loginUser = prin.getName();
+    User user = userMapper.selectById(id);
+    model.addAttribute("name", loginUser);
+    model.addAttribute("opp", user);
+    return "match.html";
+  }
+
+  @GetMapping("fight")
+  public String fight(Principal prin, @RequestParam String hand, @RequestParam int id, ModelMap model) {
+    String loginUser = prin.getName();
+    User opp = userMapper.selectById(id);
+    User user = userMapper.selectByName(loginUser);
+    Match match = new Match();
+    String result = "Draw";
+
+    match.setUser1(user.getId());
+    match.setUser2(id);
+    match.setUser1Hand(hand);
+    match.setUser2Hand("Gu");
+    matchMapper.insertMatches(match);
+
+    if (hand.equals("Pa")) {
+      result = "You Win!";
+    } else if (hand.equals("Ty")) {
+      result = "You lose";
+    }
+
+    model.addAttribute("result", result);
+    model.addAttribute("hand", hand);
+    model.addAttribute("opphand", "Gu");
+    model.addAttribute("opp", opp);
+    model.addAttribute("name", loginUser);
+
+    return "match.html";
   }
 }
--- src/main/java/oit/is/z2453/kaizi/janken/model/UserMapper.java
+++ src/main/java/oit/is/z2453/kaizi/janken/model/UserMapper.java
@@ -2,14 +2,18 @@ package oit.is.z2453.kaizi.janken.model;
 
 import java.util.ArrayList;
 
-//import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-//import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
 
-  @Select("SELECT id,name from users")
+  @Select("SELECT * FROM users")
   ArrayList<User> selectAllUser();
+
+  @Select("SELECT * FROM users where id = #{id}")
+  User selectById(int id);
+
+  @Select("SELECT * FROM users where name = #{name}")
+  User selectByName(String name);
 }
